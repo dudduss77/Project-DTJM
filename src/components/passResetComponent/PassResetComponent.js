334ea2db05be0e9ba07f1a56f377017ded78da7c
Damian Karbowiak <42693193+dudduss77@users.noreply.github.com>
2021-05-02 14:25:02 +0200

    add validator to login and resetpass component

--- src/components/passResetComponent/PassResetComponent.js
+++ src/components/passResetComponent/PassResetComponent.js
@@ -1,24 +1,41 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 import ButtonComponent from "../buttonComponent/ButtonComponent";
 import InputComponent from "../inputComponent/InputComponent";
-import ErrorComponent from '../errorComponent/ErrorComponent'
+import ErrorComponent from "../errorComponent/ErrorComponent";
+
+import { useEmailValidation } from "../../hook/emailValidator";
 
 const PassResetComponent = () => {
-  const [emailPassReset, setEmailPassReset] = useState("")
+  const [validateMessage, setValidateMessage] = useState("");
+  const [emailPassReset, setEmailPassReset] = useState("");
+  const [isEmailResetSubmitting, setIsEmailResetSubmitting] = useState(false);
+
+  const [emailValidMessage] = useEmailValidation({
+    email: emailPassReset,
+    isSubmitting: isEmailResetSubmitting,
+  });
+
+  useEffect(() => {
+    setValidateMessage(emailValidMessage);
+  }, [emailValidMessage]);
 
   const submitPassReset = () => {
-    console.log(emailPassReset);
+    if (emailValidMessage === "" && isEmailResetSubmitting) {
+      console.log(emailPassReset);
+    } else setValidateMessage("Sprawdź wszystkie pola");
+
     //API ITD.
-  }
+  };
   return (
     <>
-      <ErrorComponent errorMsg="test"/>
+      <ErrorComponent errorMsg={validateMessage} />
       <InputComponent
         size="mid"
         type="text"
         placeholder="Email"
         getValue={setEmailPassReset}
+        isSubmitting={() => setIsEmailResetSubmitting(true)}
       />
       <ButtonComponent
         size="small"

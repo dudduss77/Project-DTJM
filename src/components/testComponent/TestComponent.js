3c6cb5afed3c6c3209f96221424a21b1a3b146f5
jaroslawBinczyk <biniox2@gmail.com>
2021-05-03 17:07:59 +0200

    just only test file repair

--- src/components/testComponent/TestComponent.js
+++ src/components/testComponent/TestComponent.js
@@ -39,7 +39,7 @@ const TestComponent = () => {
 
      <UserInfoComponent/>
 
-      <UserInfoComponent/>
+      {/* <UserInfoComponent/> */}
       <PopupTestComponent/>
       <SearchMainPageTestComponent/>
     </div>
